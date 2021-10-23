01a3be5616d15b6f1a935f80480ab4700e2485b5
Sambo Chea <sombochea@cubetiqs.com>
2021-10-23 08:50:47 +0700

    Add article jpa adapter

---
+++ src/main/kotlin/com/cubetiqs/hexagonal/infrastructure/adapter/persistence/ArticleJpaDataAdapter.kt
@@ -0,0 +1,20 @@
+package com.cubetiqs.hexagonal.infrastructure.adapter.persistence
+
+import com.cubetiqs.hexagonal.domain.article.model.Article
+import com.cubetiqs.hexagonal.domain.article.port.ArticlePort
+import com.cubetiqs.hexagonal.domain.article.usecase.ArticleCreate
+import com.cubetiqs.hexagonal.domain.article.usecase.ArticleQuery
+
+class ArticleJpaDataAdapter : ArticlePort {
+    override fun create(article: ArticleCreate): Article {
+        TODO("Not yet implemented")
+    }
+
+    override fun retrieve(id: Long): Article {
+        TODO("Not yet implemented")
+    }
+
+    override fun query(query: ArticleQuery): Collection<Article> {
+        return emptyList()
+    }
+}
